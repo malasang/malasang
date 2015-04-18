e7b742d176543c825822e023dabef66a44556dd7
忠恕 <hemiao.hm@alibaba-inc.com>
2015-04-18 18:43:52 +0800

    Initial Commit

---
+++ everything/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  everything
+//
+//  Created by zhongshu on 15-4-18.
+//  Copyright (c) 2015年 malasang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ everything/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  everything
+//
+//  Created by zhongshu on 15-4-18.
+//  Copyright (c) 2015年 malasang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
